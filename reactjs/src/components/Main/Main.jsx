c062fe228cfad04020661edaca74dacbf55238b8
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-11-05 11:52:20 -0800

    commented code

--- reactjs/src/components/Main/Main.jsx
+++ reactjs/src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Register, SignIn, ErrDialog, Home } from "../components";
 import { Route, Redirect, Switch } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
+import EmptyPage from "../EmptyPage/EmptyPage";
 
 var ProtectedRoute = ({ component: Cmp, path, ...rest }) => {
     return (
